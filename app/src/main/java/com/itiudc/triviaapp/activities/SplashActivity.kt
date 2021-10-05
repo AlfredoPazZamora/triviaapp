e4577fd62ac04972010e8b48356dae4fb927f93e
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2021-10-05 07:58:31 -0500

    Diseño de login y splash

---
+++ app/src/main/java/com/itiudc/triviaapp/activities/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.itiudc.triviaapp.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.itiudc.triviaapp.R
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000) //Delay 3 seconds top open MainActivity
+    }
+}
